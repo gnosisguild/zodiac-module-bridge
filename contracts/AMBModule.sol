176aa26ae1e5ab65356e509bf4fa090b0524d041
cbrzn <cesarbrazon10@gmail.com>
2021-09-04 21:13:40 +0200

    chore: updated zodiac package name

--- contracts/AMBModule.sol
+++ contracts/AMBModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import "@gnosis/zodiac/contracts/core/Module.sol";
+import "@zodiacdao/zodiac/contracts/core/Module.sol";
 
 interface IAMB {
     function messageSender() external view returns (address);
